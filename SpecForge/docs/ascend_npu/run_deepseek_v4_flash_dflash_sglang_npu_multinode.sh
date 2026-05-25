618224ffdf3d0541c8a3e1c294c7414647a32beb
m84379596 <mohammad.mahdi.moradi@h-partners.com>
2026-05-25 23:07:05 +0800

    [launch] Add --resume flag for restart from existing checkpoint

--- SpecForge/docs/ascend_npu/run_deepseek_v4_flash_dflash_sglang_npu_multinode.sh
+++ SpecForge/docs/ascend_npu/run_deepseek_v4_flash_dflash_sglang_npu_multinode.sh
@@ -141,6 +141,7 @@ torchrun \
     --loss-decay-gamma "$LOSS_DECAY_GAMMA" \
     --log-interval "$LOG_INTERVAL" \
     --save-interval "$SAVE_INTERVAL" \
+    --resume \
     --report-to tensorboard
     # --report-to wandb \
     # --wandb-project "$WANDB_PROJECT" \
